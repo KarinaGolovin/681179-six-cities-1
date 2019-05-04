4c39c5a43afd3b9ad111c0d19c98c39d4cf09c56
Karina Golovin <karina.golovin@gmail.com>
2019-05-04 18:30:54 +0300

    PlaceCard functional test created

--- src/components/place-card/place-card.e2e.test.js
+++ src/components/place-card/place-card.e2e.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import PlaceCard from '"../place-card/place-card.jsx';
+import PlaceCard from './place-card.jsx';
 
 Enzyme.configure({adapter: new Adapter()});
 
@@ -14,7 +14,7 @@ it(`PlaceCard title link works correctly`, () => {
       />
   );
 
-  component.find(`place-card__name`).simulate(`click`);
+  component.find(`.place-card__name a`).simulate(`click`);
 
   expect(clickHandler).toHaveBeenCalledTimes(1);
 });
