98424a56f0fbf669cab3db5f890e67e34e963517
Karina Golovin <karina.golovin@gmail.com>
2019-05-04 11:41:06 +0300

    Constants moved to separate file

---
+++ src/settings.js
@@ -0,0 +1,5 @@
+const Settings = {
+  CARD_TITLES: [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`]
+};
+
+export default Settings;
