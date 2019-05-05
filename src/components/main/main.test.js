9fb9738d94acfadaf25c44617a1208066a49b1fa
Karina Golovin <karina.golovin@gmail.com>
2019-05-05 12:51:38 +0300

    Fixed Main test

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -4,11 +4,7 @@ import Main from './main.jsx';
 
 it(`Main correctly renders`, () => {
   const component = renderer.create(
-      <Main
-        title={`Some text`}
-        key={`Some text`}
-        onClick={jest.fn()}
-      />
+      <Main/>
   ).toJSON();
 
   expect(component).toMatchSnapshot();
