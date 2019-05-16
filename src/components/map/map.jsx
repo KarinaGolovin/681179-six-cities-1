ac67bd0bfdcb37e5e1ecb45377edc81a457d7d4d
Karina Golovin <karina.golovin@gmail.com>
2019-05-16 17:47:58 +0300

    Map component changed to PureComponent with ref

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,54 +1,69 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import L from 'leaflet';
 
-const Map = (props) => {
-  const {city, placesList} = props;
-  const cityCoordinates = city;
-  const mapZoom = 12;
-  const offersCords = placesList.map((place) => {
-    return place.coordinates;
-  });
-  // const offerCords = [52.3709553943508, 4.89309666406198];
-  const map = L.map(`map`, {
-    center: cityCoordinates,
-    zoom: mapZoom,
-    zoomControl: false,
-    marker: true,
-    layers: [
-      L
-        .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-          attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-        }),
-    ]
-  });
-  const mapIcon = L.icon({
-    iconUrl: `img/pin.svg`,
-    iconSize: [30, 30]
-  });
+class Map extends PureComponent {
+  constructor(props) {
+    super(props);
+    const {city, placesList} = this.props;
 
-  map.setView(cityCoordinates, mapZoom);
+    this.cityCoordinates = city;
+    this.mapZoom = 12;
+    this.offersCoordsList = placesList.map((place) => {
+      return place.coordinates;
+    });
+    this._mapRef = React.createRef();
+  }
 
-  // L
-  //   .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-  //     attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-  //   })
-  // .addTo(map);
+  render() {
+    return (
+      <div id="map" ref={this._mapRef} style={{height: `100%`}}></div>
+    );
+  }
 
-  offersCords.map((offerCords) => {
-    return L
-      .marker(offerCords, {mapIcon})
-      .addTo(map);
-  });
+  componentDidMount() {
+    const map = L.map(this._mapRef.current, {
+      center: this.cityCoordinates,
+      zoom: this.mapZoom,
+      zoomControl: false,
+      marker: true,
+      layers: [
+        L
+          .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+            attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+          }),
+      ]
+    });
 
-  return (
-    <div id="map"></div>
-  );
-};
+    const mapIcon = L.icon({
+      iconUrl: `img/pin.svg`,
+      iconSize: [30, 30]
+    });
+
+    map.setView(this.cityCoordinates, this.mapZoom);
+
+    this.offersCoordsList.map((offerCoords) => {
+      return L
+        .marker(offerCoords, {mapIcon})
+        .addTo(map);
+    });
+  }
+
+}
 
 Map.propTypes = {
   city: PropTypes.array.isRequired,
-  placesList: PropTypes.arrayOf(PropTypes.object).isRequired,
+  placesList: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    type: PropTypes.string,
+    price: PropTypes.number,
+    picture: PropTypes.string,
+    link: PropTypes.string,
+    rating: PropTypes.number,
+    isPremium: PropTypes.bool,
+    id: PropTypes.string,
+    coordinates: PropTypes.array,
+  })).isRequired,
 };
 
 export default Map;
