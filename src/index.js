0df897a50fd187b700dccee61632ca5b6966c293
Karina <karinadolls@gmail.com>
2019-06-07 20:34:33 +0300

    Fix crash in case if redux devtools not isstalled

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import {compose} from 'recompose';
 import {configureAPI} from './api';
 import rootReducer from './store/reducers';
 import {getOfferList} from './store/actions';
 import App from './components/app/app.jsx';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const init = () => {
   const api = configureAPI();
-  const store = createStore(rootReducer, compose(
-      applyMiddleware(thunk.withExtraArgument(api)),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  const store = createStore(rootReducer, composeEnhancers(
+      applyMiddleware(thunk.withExtraArgument(api))
   ));
 
   store.dispatch(getOfferList());
