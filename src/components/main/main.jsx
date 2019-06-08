51fb7606e19f48d61281c4046af676ece49db420
Karina <karinadolls@gmail.com>
2019-06-08 21:55:56 +0300

    Space added after Sort By label

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -33,6 +33,7 @@ export const Main = (props) => {
                 <b className="places__found">{currentPlaces.length} places to stay in {currentCity}</b>
                 <form className="places__sorting" action="#" method="get">
                   <span className="places__sorting-caption">Sort by</span>
+                  {` `}
                   <span className="places__sorting-type" tabIndex="0">
                     Popular
                     <svg className="places__sorting-arrow" width="7" height="4">
