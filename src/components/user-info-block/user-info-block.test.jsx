1e080b55ff18a72da7563ad75af344f6e86b4b76
Karina <karinadolls@gmail.com>
2019-06-20 13:17:57 +0300

    User info test

---
+++ src/components/user-info-block/user-info-block.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {UserInfoBlock} from './user-info-block.jsx';
+
+const mock = {
+  avatar: ``,
+  email: ``,
+}
+
+it(`UserInfoBlock correctly renders`, () => {
+  const component = renderer.create(
+      <UserInfoBlock
+        avatar={mock.avatar}
+        email={mock.email}
+      />
+  ).toJSON();
+
+  expect(component).toMatchSnapshot();
+});
