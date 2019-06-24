71625d6a99a8efc17897195422352f929e825248
Karina <karinadolls@gmail.com>
2019-06-24 11:56:24 +0300

    Form review clear state fix

--- src/components/review-form/review-form.jsx
+++ src/components/review-form/review-form.jsx
@@ -15,19 +15,15 @@ export const ReviewForm = ({
   isValid,
   onFormChange,
   onFormSubmit,
-  setFormRef,
   rules = {
     minLength: MIN_LENGTH,
     maxLength: MAX_LENGTH,
   },
-  isDisabled = false
+  isDisabled = false,
+  form
 }) => {
   return (
-    <form className="reviews__form form" action="#" method="post"
-      ref={setFormRef}
-      onChange={onFormChange}
-      onSubmit={onFormSubmit}
-    >
+    <form className="reviews__form form" action="#" method="post" onSubmit={onFormSubmit}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
         {
@@ -35,7 +31,15 @@ export const ReviewForm = ({
             const star = arr.length - index;
             return (
               <Fragment key={star}>
-                <input className="form__rating-input visually-hidden" name="rating" value={star} id={`${star}-stars`} type="radio" />
+                <input
+                  onChange={onFormChange}
+                  className="form__rating-input visually-hidden"
+                  name="rating"
+                  value={star}
+                  id={`${star}-stars`}
+                  type="radio"
+                  checked={star === form.rating}
+                />
                 <label htmlFor={`${star}-stars`} className="reviews__rating-label form__rating-label" title={title}>
                   <svg className="form__star-image" width="37" height="33">
                     <use xlinkHref="#icon-star"/>
@@ -47,12 +51,14 @@ export const ReviewForm = ({
         }
       </div>
       <textarea
+        onChange={onFormChange}
         className="reviews__textarea form__textarea"
         id="review"
         name="review"
         placeholder="Tell how was your stay, what you like and what can be improved"
         minLength={rules.minLength}
         maxLength={rules.maxLength}
+        value={form.review}
         required
       />
       <div className="reviews__button-wrapper">
@@ -74,6 +80,14 @@ export default compose(
       rating: null,
       review: ``,
     }),
+    withHandlers({
+      resetForm: (props) => () => {
+        props.setForm({
+          rating: null,
+          review: ``,
+        });
+      },
+    }),
     withFormValidation({
       review: (value, rules) => value.length >= rules.minLength && value.length <= rules.maxLength,
       rating: (value) => value > 0
@@ -81,32 +95,34 @@ export default compose(
       minLength: MIN_LENGTH,
       maxLength: MAX_LENGTH,
     }),
-    withHandlers(() => {
-      let formRef = null;
+    withHandlers({
+      onFormChange: (props) => (evt) => {
+        const fieldName = evt.target.name;
+        const value = evt.target.value;
+        const valueConverters = {
+          rating: Number
+        };
 
-      return {
-        onFormChange: (props) => (evt) => {
-          props.setForm({
-            ...props.form,
-            [evt.target.name]: evt.target.value
-          });
-        },
-        onFormSubmit: (props) => (evt) => {
-          evt.preventDefault();
-          props.onSubmitRating({
-            offerId: props.offerId,
-            rating: props.form.rating,
-            review: props.form.review,
-          });
-          formRef.reset();
-        },
-        setFormRef: () => (ref) => {
-          formRef = ref;
-        }
-      };
+        props.setForm({
+          ...props.form,
+          [fieldName]: valueConverters[fieldName] ? valueConverters[fieldName](value) : value
+        });
+      },
+      onFormSubmit: (props) => (evt) => {
+        evt.preventDefault();
+        props.onSubmitRating({
+          offerId: props.offerId,
+          rating: props.form.rating,
+          review: props.form.review,
+        });
+        props.resetForm();
+      }
     }),
     lifecycle({
-      componentDidUpdate() {
+      componentDidUpdate(prevProps) {
+        if (this.props.offerId !== prevProps.offerId) {
+          this.props.resetForm();
+        }
         this.props.validateForm({
           rating: this.props.form.rating,
           review: this.props.form.review,
@@ -118,11 +134,14 @@ export default compose(
 ReviewForm.propTypes = {
   onFormChange: PropTypes.func.isRequired,
   onFormSubmit: PropTypes.func.isRequired,
-  setFormRef: PropTypes.func.isRequired,
   isValid: PropTypes.bool.isRequired,
   rules: PropTypes.shape({
     minLength: PropTypes.number,
     maxLength: PropTypes.number,
   }),
-  isDisabled: PropTypes.bool
+  isDisabled: PropTypes.bool,
+  form: PropTypes.shape({
+    review: PropTypes.string,
+    rating: PropTypes.oneOf([1, 2, 3, 4, 5])
+  })
 };
